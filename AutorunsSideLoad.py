c0d5709d1630c3565437ef2b3ca4d6f97b5b5f96
tranggithub <100346202+tranggithub@users.noreply.github.com>
2025-07-07 02:05:46 +0700

    Add files via upload

--- AutorunsSideLoad.py
+++ AutorunsSideLoad.py
@@ -2,7 +2,9 @@ import os
 import csv
 import hashlib
 import subprocess
+from colorama import init, Fore, Style
 
+init(autoreset=True)  # Tự reset về màu thường sau mỗi dòng
 SIGCHECK_PATH = "..\SysSuite\sigcheck.exe"  # Đường dẫn đến sigcheck.exe
 
 def get_md5(file_path):
@@ -14,11 +16,20 @@ def get_md5(file_path):
 
 def get_sigcheck_info(file_path):
     try:
-        result = subprocess.run([SIGCHECK_PATH, "-n", "-q", file_path], capture_output=True, text=True)
+        result = subprocess.run([SIGCHECK_PATH, file_path], capture_output=True, text=True)
         output = result.stdout.strip()
-        if output:
-            return output.splitlines()[0]  # Lấy dòng đầu (signature info)
-        return "Unknown"
+        lines = output.splitlines()
+
+        verified = "Unknown"
+        publisher = "Unknown"
+
+        for line in lines:
+            if "Verified:" in line:
+                verified = line.split(":", 1)[1].strip()
+            elif "Publisher:" in line:
+                publisher = line.split(":", 1)[1].strip()
+
+        return f"{verified} {publisher}"
     except Exception as e:
         return f"Error: {str(e)}"
 
@@ -32,22 +43,38 @@ def get_all_files_in_folder(folder_path):
 def extract_dir(image_path):
     return os.path.dirname(image_path.strip('"'))
 
+def is_pe_file(file_path):
+    try:
+        ext = os.path.splitext(file_path)[1].lower()
+        if ext not in ['.dll', '.exe']:
+            return False
+        with open(file_path, 'rb') as f:
+            magic = f.read(2)
+            return magic == b'MZ'
+    except:
+        return False
 def main(autoruns_input_csv, output_csv):
     results = []
-
-    with open(autoruns_input_csv, newline='', encoding='utf-8') as csvfile:
+    folders = []
+    with open(autoruns_input_csv, newline='', encoding='utf-8-sig',errors='replace') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             image_path = row.get("Image Path")
             if not image_path or not os.path.isfile(image_path):
                 continue
-
             base_name = os.path.basename(image_path)
             folder = extract_dir(image_path)
+            if (folder in folders):
+                continue
+            folders.append(folder)
+            if (folder == 'c:\windows\system32' or folder == 'c:\windows\syswow64'):
+                continue
+            print(Fore.YELLOW + f"{folder}")
             files = get_all_files_in_folder(folder)
 
             for f in files:
-                print(f)
+                if not is_pe_file(f):
+                    continue
                 sig_info = get_sigcheck_info(f)
                 md5 = get_md5(f)
                 results.append({
@@ -56,6 +83,11 @@ def main(autoruns_input_csv, output_csv):
                     "Digital Signature": sig_info,
                     "MD5": md5
                 })
+                
+#                if sig_info not in whitelists:
+#                    print(f"{base_name}, {f}, {sig_info}, {md5}")
+                if "unsigned " in sig_info.lower():
+                    print(f"{base_name}, {f}, {sig_info}, {md5}")
 
     with open(output_csv, 'w', newline='', encoding='utf-8') as csv_out:
         writer = csv.DictWriter(csv_out, fieldnames=["Original Image Name", "Collected File Path", "Digital Signature", "MD5"])
@@ -67,4 +99,5 @@ def main(autoruns_input_csv, output_csv):
 if __name__ == "__main__":
     autoruns_input_csv = "D:\Viettel\Lab1\windows\samples_DESKTOP-92OTEJ5___20250705_200731.573\log_autoruns.csv"  # Đầu vào từ Autoruns
     output_csv = "collected_file_info.csv"
+    whitelists = ["Signed Microsoft Corporation","Signed Microsoft Windows Publisher"]
     main(autoruns_input_csv, output_csv)
